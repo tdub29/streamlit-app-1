fddfcbd30eb510c0777aaf3cf44b8c0a635ff40d
tdub29 <108891102+tdub29@users.noreply.github.com>
2024-12-28 15:57:15 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -311,6 +311,8 @@ df["datetime"] = (
 df['Pitchtype'] = df['Taggedpitchtype'].replace('Undefined', np.nan).fillna(df['Autopitchtype'])
 df['Pitchtype'] = df['Pitchtype'].replace(['Four-Seam', 'FourSeamFastBall'], 'Fastball')
 
+df.to_csv('data/streamlit_2024_fall_data.csv', index=False)
+
 
 # Convert 'Tilt' column from HH:MM format to float (1:45 -> 1.75)
 def convert_tilt_to_float(tilt_value):
